354ee354c2a541e94a3f718dc16b67b65adfbbd1
wyle09 <wylecordero@gmail.com>
2019-05-25 18:39:11 -0400

    First commit

---
+++ javascript/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    $('#slides').superslides({
+        animation: 'fade',
+        play: 5000,
+        pagination: false
+    });
+
+    var typed = new Typed(".typed", {
+        strings: ["Process Optimization", "Software Package Selection", "Benefit Consulting"],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 2
+            },
+            768: {
+                items: 3                
+            },
+            938: {
+                items: 4
+            }
+        }
+    });
+});
